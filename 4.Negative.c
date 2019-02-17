a4b459692ee361f0554a10f107a5da30028169f7
“Mostafiz24” <Mostafizs154@gmail.com>
2019-02-17 22:08:56 +0600

    Negative Image

---
+++ 4.Negative.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include<math.h>
+
+#define ROW 500
+#define COL 500
+
+int row,col,maxgray;
+int in_image [ROW][COL];
+int out_image [ROW][COL];
+
+int ROWS, COLS;
+
+
+int read(char *filename)
+{
+    int i,j,nl=0;
+    char ch;
+    FILE *fp;
+    fp=fopen(filename,"rb");
+
+// Extracting Header information
+    while (nl<2)
+    {
+        fscanf(fp, "%c",&ch);
+        if (ch=='\n')
+            nl++;
+    }
+    fscanf(fp,"%d%d%d",&col,&row,&maxgray);
+
+    // Reading data from file
+    for(i=0; i<row; i++)
+    {
+        for (j=0; j<col; j++)
+        {
+            fscanf (fp, "%d", &in_image[i][j]);
+
+        }
+    }
+    fclose(fp);
+}
+
+int write(char *filename)
+{
+    int i,j,nl=0;
+    char ch;
+    FILE *fp;
+    fp=fopen(filename,"wb");
+
+// Writing header into fp
+    fprintf (fp,"%s\n%s\n%d %d\n%d\n",
+             "P2","# Created By Mostafiz",col,row,255);
+
+    //Putting data into another file
+    for (i=0; i<row; i++)
+    {
+        for (j=0; j<col; j++)
+        {
+            fprintf(fp, "%d ", out_image[i][j]);
+        }
+        fprintf (fp,"%s","\n");
+    }
+
+
+    fclose(fp);
+
+}
+
+int main()
+{
+    int i,j;
+    read("abcd.pgm");
+
+    ///Processing the image
+    for (i=0; i<row; i++)
+    {
+        for (j=0; j<col; j++)
+        {
+            out_image[i][j] = maxgray - in_image[i][j];
+
+        }
+
+    }
+    write("4.Negative.pgm");
+
+
+    return 0;
+
+
+}
+
+
